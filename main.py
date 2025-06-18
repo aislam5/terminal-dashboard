347c76eb53bf3c5fc4cbd5db81ceb28dc1a49730
Abdullah Islam <abdullahislam900@gmail.com>
2025-06-18 17:26:47 -0400

    main.oy and the stages to learning and implementing this

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+1. Learn how to fetch wether data using the OpenWeatherMap API
+2. USe NewsAPI.org to fetch top headlines
+3. Use date and time module 
+4. Use some free quote API to fetch daily inspirational quotes
+5. make the dashboard look good with the rich library
+"""
+
